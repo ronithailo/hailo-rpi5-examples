1e865827d4bb8e0043306ca105a637fc1100d22f
ronithailo <ronits@hailo.ai>
2025-01-24 14:07:17 +0000

    face_recognition

--- basic_pipelines/face_recognition/face_recognition.py
+++ basic_pipelines/face_recognition/face_recognition.py
@@ -11,7 +11,7 @@ from hailo_apps_infra.hailo_rpi_common import (
     get_numpy_from_buffer,
     app_callback_class,
 )
-from hailo_apps_infra.face_recognition_pipeline import GStreamerFaceRecognitionApp
+from hailo_apps_infra.face_recognition.face_recognition_pipeline import GStreamerFaceRecognitionApp
 
 # -----------------------------------------------------------------------------------------------
 # User-defined class to be used in the callback function
@@ -68,16 +68,6 @@ def app_callback(pad, info, user_data):
                 track_id = track[0].get_id()
             string_to_print += (f"Detection: ID: {track_id} Label: {label} Confidence: {confidence:.2f}\n")
             detection_count += 1
-    if user_data.use_frame:
-        # Note: using imshow will not work here, as the callback function is not running in the main thread
-        # Let's print the detection count to the frame
-        cv2.putText(frame, f"Detections: {detection_count}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        # Example of how to use the new_variable and new_function from the user_data
-        # Let's print the new_variable and the result of the new_function to the frame
-        cv2.putText(frame, f"{user_data.new_function()} {user_data.new_variable}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        # Convert the frame to BGR
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        user_data.set_frame(frame)
 
     print(string_to_print)
     return Gst.PadProbeReturn.OK
